23e1201f1a436874c0e24fbe68c45819c39ca6fd
CozzyBro2 <gojinhan2@gmail.com>
2022-08-11 20:58:35 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# pineapples-bot
+Multi-purpose discordia discord bot
+
+I love pineapples :3
---
+++ package.lua
@@ -0,0 +1,16 @@
+  return {
+    name = "pineapplesbot",
+    version = "0.0.1",
+    description = "Multi-purpose discordia discord bot",
+    tags = { "discord", "discordia", "bot" },
+    license = "The Unlicense",
+    author = { name = "CozzyBro2", email = "gojinhan2@gmail.com" },
+    homepage = "https://github.com/pineapples-bot",
+    dependencies = {
+      "SinisterRectus/discordia@2.9.2"
+    },
+    files = {
+      "**.lua",
+      "!test*"
+    }
+  }
