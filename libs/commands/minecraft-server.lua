bf4ff4cbf20138dddf9612a8fab26acdf0cf051d
CozzyBro2 <gojinhan2@gmail.com>
2022-12-12 13:16:48 -0500

    make it suck less

--- libs/commands/minecraft-server.lua
+++ libs/commands/minecraft-server.lua
@@ -1,8 +1,9 @@
 local module = {}
 
+local http = require('http')
 local https = require('https')
-local json = require('json')
 
+local json = require('json')
 local enums = require("discordia-slash").enums
 
 --local api_format = 'https://api.mcsrvstat.us/2/%s'
@@ -28,10 +29,25 @@ module.options = {
     },
 }
 
-local function HandleError(err)
-    print(err)
-
-    error("Encountered error when making request.", 0)
+-- Darius, marc_s: Stack overflow
+local function httpGET(url, callback)
+    url = http.parseUrl(url)
+    local req = (url.protocol == 'https' and https or http).get(url, function(res)
+      local body={}
+      res:on('data', function(s)
+        body[#body+1] = s
+      end)
+      res:on('end', function()
+        res.body = table.concat(body)
+        callback(res)
+      end)
+      res:on('error', function(err)
+        callback(res, err)
+      end)
+    end)
+    req:on('error', function(err)
+      callback(nil, err)
+    end)
 end
 
 function module.callback(interaction, params)
@@ -43,66 +59,70 @@ function module.callback(interaction, params)
 
     local url = string.format(api_format, ip)
 
-    local request = https.get(url, function(result)
-        result:on('data', function(str)
-            local data = json.parse(str)
+    print('Making a request to:', url)
 
-            local isOnline = not not data.online
+    local function SendEmbed(data)
+        local isOnline = not not data.online
 
-            local uptime = data.duration
-            local uptimeDays = tonumber(uptime) / 86400
+        local uptime = data.duration
+        local uptimeDays = tonumber(uptime) / 86400
 
-            local serverInfo = data.server
-            local playersInfo = data.players
+        local serverInfo = data.server
+        local playersInfo = data.players
 
-            coroutine.resume(coroutine.create(function()
-                interaction:reply {
-                    embed = {
-                        title = ip,
-                        description = 'Here is information about that server:',
-                        url = url,
+        interaction:reply {
+            embed = {
+                title = ip,
+                description = 'Here is information about that server:',
+                url = url,
             
-                        fields = {
-                            {
-                                name = 'Is Online:',
-                                value = isOnline or default_field
-                            },
+                fields = {
+                    {
+                        name = 'Is Online:',
+                        value = isOnline
+                    },
 
-                            {
-                                name = 'Version:',
-                                value = serverInfo.name or default_field
-                            },
+                    {
+                        name = 'Version:',
+                        value = serverInfo.name or default_field
+                    },
 
-                            {
-                                name = 'Players Online:',
-                                value = playersInfo.now or default_field
-                            },
+                    {
+                        name = 'Players Online:',
+                        value = playersInfo.now or default_field
+                    },
 
-                            {
-                                name = "Uptime (probably bogus):",
-                                value = string.format(uptime_format, uptimeDays, (uptime / 3600) % 24)
-                            }
-                            
-                        },
-            
-                        author = {
-                            name = 'Server Information',
-                        },
-
-                        thumbnail = data.favicon and {
-                            url = tostring(data.favicon),
-                        },
-            
-                        color = 0x00ff00,
+                    {
+                        name = "Uptime (probably bogus):",
+                        value = string.format(uptime_format, uptimeDays, (uptime / 3600) % 24)
                     }
-                }
-            end))
-        end)
+       
+                },
 
-        result:on('error', HandleError)
+                author = {
+                    name = 'Server Information',
+                },
+
+                --[[
+                thumbnail = data.favicon and {
+                    url = tostring(data.favicon),
+                },
+                --]]
+            
+                color = 0x00ff00,
+            }
+        }
+    end
+
+    httpGET(url, function(result, err)
+        if err then
+            error(err, 0)
+        else
+            local data = json.parse(result.body)
+
+            coroutine.wrap(SendEmbed)(data)
+        end
     end)
-
-    request:on('error', HandleError)
 end
 
 return module
